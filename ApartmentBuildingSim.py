1cb41b583cbf3f24386f87d05f5afdf6220825c5
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-06-27 22:45:33 -0400

    another step towards succss

--- ApartmentBuildingSim.py
+++ ApartmentBuildingSim.py
@@ -8,6 +8,15 @@ from termcolor import *
 jdb = jsoneng.JsonDB()
 jdb.create({})
 
+
+
+
+
+
+
+
+
+
 class Char:
     def __init__(self, name):
         self.name = name
@@ -22,10 +31,21 @@ class WaterDrinker(Char):
         if self.state == 'idle':
             heapq.heappush(futureEventList, DrinkWater0(simTime + random.randint(1,4)))
 
+
+
+
+
+
+
+
+
+
 class Event:
-    def __init__(self, time, name):
+    def __init__(self, time, chars, ):
         self.time = time
         self.name = name
+        self.futureEventList = futureEventList
+        self.chars = chars
 
     def __repr__(self):
         return f"{colored('EVENT:','red')} time:{self.time} name:{self.name}"
@@ -34,30 +54,41 @@ class Event:
         return self.time < other.time
     
 class InitCharsEvent(Event):
-    def __init__(self, time):
-        super().__init__(time, 'init chars')
+    def __init__(self, time, chars):
+        super().__init__(time, chars, 'init chars')
 
-    def handleEvent(self, futureEventList, chars):
-        for c in chars:
+    def handleEvent(self, futureEventList):
+        for c in self.chars:
             c.state = 'idle'
             c.location = 'lobby'
     
 class DrinkWater0(Event):
-    def __init__(self, time):
-        super().__init__(time, 'stage 0: start drinking water')
+    def __init__(self, time, chars):
+        super().__init__(time, chars, 'stage 0: start drinking water')
 
-    def handleEvent(self, futureEventList, chars):
-        for c in chars:
+    def handleEvent(self, futureEventList):
+        for c in self.chars:
             c.state = 'stage 0: drink water'
-        heapq.heappush(futureEventList, DrinkWater1(self.time))
+        heapq.heappush(self.futureEventList, DrinkWater1(self.time, self.chars))
 
 class DrinkWater1(Event):
-    def __init__(self, time):
-        super().__init__(time, 'stage 1: finish drinking water')
+    def __init__(self, time, chars):
+        super().__init__(time, chars, 'stage 1: finish drinking water')
 
-    def handleEvent(self, futureEventList, chars):
-        for c in chars:
+    def handleEvent(self, futureEventList):
+        for c in self.chars:
             c.state = 'idle'
+
+
+
+
+
+
+
+
+
+
+
 
 chars = []
 harry = WaterDrinker('Harry')
@@ -66,15 +97,15 @@ chars.append(harry)
 chars.append(alice)
 
 fel = []
-initCharsEvent = InitCharsEvent(0)
+initCharsEvent = InitCharsEvent(0, chars)
 fel.append(initCharsEvent)
 
 simTime = 0
-while simTime < 10:
+while simTime < 100:
     currEvent = heapq.heappop(fel)
     simTime += currEvent.time
 
-    currEvent.handleEvent(fel, chars)
+    currEvent.handleEvent(fel)
 
     for c in chars:
         c.createEvents(simTime, fel, chars)
