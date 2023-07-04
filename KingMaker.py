05670e8c4db82613831aba45bf036539af13697b
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-04 17:07:19 -0400

    fix

--- KingMaker.py
+++ KingMaker.py
@@ -1,21 +1,42 @@
+# 1D grand strategy simulator game
+
+# eras, mythical, bronze, iron, steel, gunpowder
+    # each era can have random amount of time (historical events)
+
+# great figures 1-5 (characters)
+
+# civs (cultures)
+
+# important places (cities, battlefields, monuments)
+
 import heapq
 
-class GlobalState:
-    def __init__(self, global_data: dict):
-        self.global_data = global_data
+class GlobalData:
+    def __init__(self, globalData: dict):
+        self.globalData = globalData
 
-class CharState:
-    def __init__(self, char_data: list):
-        self.char_data = char_data
+sampleGlobalData = GlobalData({'worldTension':0})
+
+class Character:
+    def __init__(self, name, charData, charState):
+        self.name = name
+        self.charData = charData
+        self.charState = charState
+
+sampleCharacter = Character('tom',{'combat':10,'speech':10,'craft':10},'IDLE')
+
+class CharsData:
+    def __init__(self, charsData: list):
+        self.charsData = charsData
 
 class Event:
-    def __init__(self, time: int, event_name: str, state_change: list):
+    def __init__(self, time: int, eventName: str, stateChanges: list):
         self.time = time
-        self.event_name = event_name
-        self.state_change = state_change
+        self.eventName = eventName
+        self.stateChanges = stateChanges
     
     def __repr__(self):
-        return f"EVENT: time:{self.time} name:{self.name}"
+        return f"Event.time:{self.time} Event.name:{self.eventName}"
 
     def __lt__(self, other):
         return self.time < other.time
@@ -26,8 +47,8 @@ class EventCreator:
         self.globalState = globalState
         self.charState = charState
 
-    def create_event(self, time: int, event_name: str):
-        event = Event(time, event_name)
+    def create_event(self, time: int, eventName: str):
+        event = Event(time, eventName)
         heapq.heappush(self.fel, (event.time, event))  # Add event to the FEL
 
 class EventResolver:
@@ -37,8 +58,8 @@ class EventResolver:
 
     def handle_event(self, event: Event):
         # Handle the event by handling the respective state changes to the character and global states
-        state_changes = event.state_change
-        for attribute, change_value in state_changes.items():
+        stateChangess = event.stateChanges
+        for attribute, change_value in stateChangess.items():
             self.update_state(attribute, change_value)
 
     def update_state(self, attribute, change_value):
@@ -49,10 +70,10 @@ class Simulation:
     def __init__(self, sim_time, fel, global_data, char_data):
         self.sim_time = sim_time
         self.fel = heapq.heapify(fel)
-        self.globalState = GlobalState(global_data)
-        self.charState = CharState(char_data)
-        self.event_creator = EventCreator(self.fel, self.globalState, self.charState)
-        self.event_resolver = EventResolver(self.globalState, self.charState)
+        self.globalData = GlobalData(global_data)
+        self.charData = CharsData(char_data)
+        self.event_creator = EventCreator(self.fel, self.globalData, self.charData)
+        self.event_resolver = EventResolver(self.globalData, self.charData)
 
     def run(self, max_time: int):
         while True:
