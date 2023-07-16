fde2f0cd020141e31192e8d98f81986fe51562a1
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-16 16:52:42 -0400

    journal and broad questions

--- prototype/PromptGames/PromptGame.py
+++ prototype/PromptGames/PromptGame.py
@@ -34,7 +34,7 @@ def randomRoll(dictionary):
     out = {}
     roll = 0
     while roll <= 80:
-        roll = roll_dice() + roll + 10
+        roll = roll_dice() + roll + 5
         part = assign_part(counts, roll)
 
         if part in dictionary:
@@ -59,6 +59,22 @@ def play(game, output):
 
     dictionary = randomRoll(jdb.retrieve(game))
 
+    q1 = "broad summary of your previous life"
+    c1 = "a character you are familiar with"
+    c2 = "another character you are familiar with"
+    s1 = "a skill you have"
+    s2 = "another skill you have"
+    r1 = "a resource you have"
+    r2 = "another resource you have"
+    e1 = "an experience you have relating to two of the things you described"
+
+    preGame = [q1, c1, c2, s1, s2, r1, r2, e1]
+
+    for i in range(len(preGame)):
+        print(colored(preGame[i], "red"))
+        text = input("> ")
+        jdb.patch({i: {"prompt": preGame[i], "answer": text}}, output)
+
     for i, (k, v) in enumerate(dictionary.items()):
         print(colored(f"prompt {k}", "light_blue"))
         print(colored(v[0], "red"))
@@ -67,4 +83,4 @@ def play(game, output):
 
 
 # create("ThousandYearOldVampire.txt", "TYOV")
-play("ALifeLived", "Play")
+play("TYOV", "Play")
