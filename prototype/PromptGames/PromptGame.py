a24351da2f1561b5295ec7457b4cbbc40a05752b
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-17 12:25:25 -0400

    reorg promptgame

--- prototype/PromptGames/PromptGame.py
+++ prototype/PromptGames/PromptGame.py
@@ -1,51 +1,26 @@
-def textToDict(textFile):
-    # for parsing the original TYOV txt file only
-    import re
-
-    dictionary = {}
-    key = ""
-
-    with open(textFile, "r", encoding="utf-8") as file:
-        for line in file:
-            line = line.rstrip()  # remove trailing newline character
-
-            # Check if the line ends with a letter indicating a new key
-            if re.match(r"[0-9]+[a-z]$", line):
-                key = line
-                dictionary[key] = []
-            else:
-                # Add the line to the current key's value list
-                dictionary[key].append(line.strip())
-
-    return dictionary
-
-
-def randomRoll(dictionary):
-    import random
-
-    def roll_dice():
-        return random.randint(1, 10) - random.randint(1, 6)
-
-    def assign_part(counts, roll):
-        current_count = counts.get(roll, 0) + 1
-        counts[roll] = current_count
-        return f"{roll}{chr(ord('a')+current_count-1)}"
-
-    counts = {}
-    out = {}
-    roll = 0
-    while roll <= 80:
-        roll = roll_dice() + roll + 5
-        part = assign_part(counts, roll)
-
-        if part in dictionary:
-            out[part] = dictionary[part]
-
-    return out
-
-
 def create(textFile, game):
     import jsoneng
+
+    def textToDict(textFile):
+        # for parsing the original TYOV txt file only
+        import re
+
+        dictionary = {}
+        key = ""
+
+        with open(textFile, "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.rstrip()  # remove trailing newline character
+
+                # Check if the line ends with a letter indicating a new key
+                if re.match(r"[0-9]+[a-z]$", line):
+                    key = line
+                    dictionary[key] = []
+                else:
+                    # Add the line to the current key's value list
+                    dictionary[key].append(line.strip())
+
+        return dictionary
 
     jdb = jsoneng.JsonDB()
     jdb.create(textToDict(textFile), game)
@@ -54,6 +29,29 @@ def create(textFile, game):
 def play(game, output):
     import jsoneng
     from termcolor import colored
+
+    def randomRoll(dictionary):
+        import random
+
+        def roll_dice():
+            return random.randint(1, 10) - random.randint(1, 6)
+
+        def assign_part(counts, roll):
+            current_count = counts.get(roll, 0) + 1
+            counts[roll] = current_count
+            return f"{roll}{chr(ord('a')+current_count-1)}"
+
+        counts = {}
+        out = {}
+        roll = 0
+        while roll <= 80:
+            roll = roll_dice() + roll + 5
+            part = assign_part(counts, roll)
+
+            if part in dictionary:
+                out[part] = dictionary[part]
+
+        return out
 
     jdb = jsoneng.JsonDB()
     jdb.create({}, output)
@@ -68,8 +66,9 @@ def play(game, output):
     r1 = "a resource you have"
     r2 = "another resource you have"
     e1 = "an experience you have relating to two of the things you described"
+    i1 = "inciting incident (for TYOV, create an immortal and mark to explain how you became a vampire)"
 
-    preGame = [q1, c1, c2, s1, s2, r1, r2, e1]
+    preGame = [q1, c1, c2, s1, s2, r1, r2, e1, i1]
 
     for i in range(len(preGame)):
         print(colored(preGame[i], "red"))
