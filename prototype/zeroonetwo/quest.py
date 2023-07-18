0ce08727ff0d32f5137c1767ba1b73de3b49c89b
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-17 22:43:22 -0400

    quest events

--- prototype/zeroonetwo/quest.py
+++ prototype/zeroonetwo/quest.py
@@ -4,16 +4,75 @@ import sys
 from dataclasses import *
 import jsoneng
 
+guardEncounter = """
+you stumbled onto some guards in the place, roll to determine
 
-events = {
-    "guard": ["change of guard", "encounter guard"],
-    "social": ["drink", "party"],
-    "omen": ["item in mirror", "vampire", "werewolf"],
-}
+roll d4
+>4: you are safe
+3: the guards saw you but not your face
+check speed >4 to escape
+2: the guards stops you for a routine inspection
+check sanity >4 to escape
+1: the guards saw you and your face
+check speed >4 to escape
+0: the guards stops you and initiate combat
+create 1 to 4 guards as enemy
+"""
 
 
-def getString(dictionary):
-    return random.choice(dictionary[random.choice(list(dictionary.keys()))])
+events = [guardEncounter]
+
+wraithInTheMirror = """
+the player next to you saw a wraith in the mirror
+it is their future self, and in order to help them
+they must discard one item to the mirror
+"""
+
+omens = [wraithInTheMirror]
+
+revolver = """
+this military patterend revolver
+is made from the best weapon smith in englandia
+
+increase grit by 2
+allows you to use speed to attack
+for that attack only, increase speed by 2
+"""
+
+silverSword = """
+a fine silver sword
+
+increase grit by 2
+"""
+
+bloodySpear = """
+a bloody spear, embued with power
+
+increase grit by 2
+"""
+
+weapons = [revolver, silverSword, bloodySpear]
+
+firstAidKit = """
+a set of military mandages
+
+heals 2 physical damage
+"""
+
+sokolovPhysicRemedy = """
+alternative medicine created by the great
+natural philosopher anton sokolov
+can heal mental damage
+
+heals 2 mental damage
+"""
+
+items = [firstAidKit, sokolovPhysicRemedy]
 
 
-print(getString(events))
+humans = []
+
+
+# print(random.choice(events))
+# print(random.choice(omens))
+print(random.choice(weapons))
