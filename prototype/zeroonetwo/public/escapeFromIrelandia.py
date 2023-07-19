b5b7babcc2bfa83cb96f807f8c0e56f5f2689e2e
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-19 15:25:00 -0400

    add gun

--- prototype/zeroonetwo/public/escapeFromIrelandia.py
+++ prototype/zeroonetwo/public/escapeFromIrelandia.py
@@ -1,4 +1,8 @@
 from dataclasses import *
+import jsoneng
+
+jdb = jsoneng.JsonDB()
+jdb.create({})
 
 # ESCAPE FROM IRELANDIA
 
@@ -20,25 +24,40 @@ class Jailor(Char):
 
 # LOCALES
 
+prison = {
+    "armory": [
+        "weapon cache",
+        "armory entrance",
+    ],
+    "yard": [
+        "main yard",
+        "yard gates",
+    ],
+}
+
 
 @dataclass
 class Locale:
     name: str
+    listOfItems: list
+    listOfGoodEvents: list
+    listOfBadEvents: list
 
 
-class Prison(Locale):
+class WeaponCache(Locale):
     def __init__(self):
-        self.name = "Irelandia"
-        self.prison = {
-            "armory": [
-                "weapon cache",
-                "armory entrance",
-            ],
-            "yard": [
-                "main yard",
-                "yard gates",
-            ],
-        }
+        self.name = "Weapon Cache"
+        self.listOfItems = []
+        self.listOfGoodEvents = []
+        self.listOfBadEvents = []
+
+
+class WeaponCache(Locale):
+    def __init__(self):
+        self.name = "Weapon Cache"
+        self.listOfItems = []
+        self.listOfGoodEvents = []
+        self.listOfBadEvents = []
 
 
 # ITEMS
@@ -47,7 +66,76 @@ class Prison(Locale):
 @dataclass
 class Item:
     name: str
+    desc: str
+    actions: list
 
+
+@dataclass
+class Action:
+    name: str
+    desc: str
+    actionRollTable: dict
+
+
+class Gun(Item):
+    def __init__(self):
+        self.name = "Gun"
+        self.desc = "This revolver contains six bullets, but it tends to jam"
+
+        class ShootFullClip(Action):
+            def __init__(self):
+                self.name = "Shoot (Full Clip)"
+                self.desc = "you take aim with your finesess and shoot, you can use this once per turn"
+                self.actionRollTable = {
+                    "rollType": "speed",
+                    "roll": [
+                        ["4", "incapicate target"],
+                        ["3", "miss"],
+                    ],
+                }
+
+        class ShootHalfClip(Action):
+            def __init__(self):
+                self.name = "Shoot (Half Clip)"
+                self.desc = "you take aim with your finesess and shoot, you can use this once per turn"
+                self.actionRollTable = {
+                    "rollType": "speed",
+                    "roll": [
+                        ["8", "incapicate target"],
+                        ["7", "miss"],
+                    ],
+                }
+
+        class Reload(Action):
+            def __init__(self):
+                self.name = "Reload"
+                self.desc = "you reload your gun, remove bullets from your inventory"
+                self.actionRollTable = {
+                    "rollType": "knowledge",
+                    "roll": [
+                        ["2", "reload succeed"],
+                        ["1", "reload failed, try again"],
+                    ],
+                }
+
+        class Aim(Action):
+            def __init__(self):
+                self.name = "Aim"
+                self.desc = "you aim your gun, allowing an easier time to fire"
+                self.actionRollTable = {
+                    "rollType": "sanity",
+                    "roll": [
+                        ["8", "increase speed +2 to your next shoot"],
+                        ["4", "increase speed +1 to your next shoot"],
+                    ],
+                }
+
+        self.actions = [ShootFullClip(), ShootHalfClip(), Reload(), Aim()]
+
+
+gun = Gun()
+
+# choice = input(f"> {[a.name for a in gun.actions]} ")
 
 # GOOD EVENTS
 
@@ -63,6 +151,3 @@ class GoodEvent:
 @dataclass
 class BadEvent:
     name: str
-
-
-print(Jailor())
