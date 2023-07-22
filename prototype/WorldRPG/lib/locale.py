ea909430a8ee89300a487f83939899dd6cdb062e
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-21 20:40:52 -0400

    world

--- prototype/WorldRPG/lib/locale.py
+++ prototype/WorldRPG/lib/locale.py
@@ -41,18 +41,67 @@ class Overworld(World):
                         self.localeName = "marsh"
                         self.desc = "marsh"
 
-                self.locales = [Beach(), Marsh()]
+                class Cliff(Locale):
+                    def __init__(self):
+                        self.localeName = "cliff"
+                        self.desc = "cliff"
+
+                class Shore(Locale):
+                    def __init__(self):
+                        self.localeName = "shore"
+                        self.desc = "shore"
+
+                self.locales = [Beach(), Marsh(), Cliff(), Shore()]
 
         class Plains(Biome):
             def __init__(self):
                 self.biomeName = "plains"
-                self.desc = "the plains"
+                self.desc = "plains"
 
                 class GrassyField(Locale):
                     def __init__(self):
                         self.localeName = "grassy field"
                         self.desc = "grassy field"
 
-                self.locales = [GrassyField()]
+                class FarmLands(Locale):
+                    def __init__(self):
+                        self.localeName = "farm lands"
+                        self.desc = "farm lands"
 
-        self.biomes = [Coast(), Plains()]
+                self.locales = [GrassyField(), FarmLands()]
+
+        class Forest(Biome):
+            def __init__(self):
+                self.biomeName = "forest"
+                self.desc = "forest"
+
+                class Woods(Locale):
+                    def __init__(self):
+                        self.localeName = "woods"
+                        self.desc = "woods"
+
+                class DeepWoods(Locale):
+                    def __init__(self):
+                        self.localeName = "deep woods"
+                        self.desc = "deep woods"
+
+                self.locales = [Woods(), DeepWoods()]
+
+        class Mountains(Biome):
+            def __init__(self):
+                self.biomeName = "mountains"
+                self.desc = "mountains"
+
+                class Hills(Locale):
+                    def __init__(self):
+                        self.localeName = "hills"
+                        self.desc = "hills"
+
+                class Mountains(Locale):
+                    def __init__(self):
+                        self.localeName = "mountains"
+                        self.desc = "mountains"
+
+                self.locales = [Hills(), Mountains()]
+
+        self.biomes = [Coast(), Plains(), Forest(), Mountains()]
