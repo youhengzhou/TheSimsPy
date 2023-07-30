5bdd473f82b4beac0be8757344d3f62296b6314d
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-30 02:05:33 -0400

    .

--- prototype/agentdriven/g.py
+++ prototype/agentdriven/g.py
@@ -19,5 +19,25 @@ class Skill:
     rolltable: dict
 
 
+@dataclass
+class Place:
+    name: str
+    rolltable: dict
+    neighbors: list
+
+
 firesword = Skill("firesword", {"close range": 10})
 rifle = Skill("rifle", {"close range": 5, "mid range": 5})
+
+
+waterfountain = Place("water fountain", {"close range": 10, "mid range": 10}, [])
+square = Place("square", {"close range": 10, "mid range": 10}, [])
+
+park = [waterfountain, square]
+
+for place in park:
+    for otherPlace in park:
+        if place != otherPlace:
+            place.neighbors.append(otherPlace)
+
+jdb.p(asdict(square))
