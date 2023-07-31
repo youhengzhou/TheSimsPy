5a2cdbf88d486b381aa90c6dc220abd3a3dde6f6
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-31 16:52:03 -0400

    .

--- prototype/realworld/main.py
+++ prototype/realworld/main.py
@@ -17,7 +17,7 @@ class PlaceMap:
 
 
 h_livingroom = Place("h_livingroom", "Living Room", False, ["h_hallway"])
-h_hallway = Place("h_hallway", "Hallway", False, ["h_livingroom"])
+h_hallway = Place("h_hallway", "Hallway", False, ["h_livingroom", "h_door"])
 h_door = Place("h_door", "Door", True, ["h_hallway"])
 
 h_map = PlaceMap(
@@ -65,36 +65,37 @@ def traverse(place_map, place_key):
     """
     Traverses through the place map starting from the given place key.
     """
-    if place_key in place_map.placeMap:
-        current_place = place_map.placeMap[place_key]
-        print(f"You are in {current_place.name}.")
 
-        if current_place.exit:
-            print("This place has an exit.")
-
-        if current_place.neighbors:
-            print("Neighbors: ")
-            for i, neighbor_key in enumerate(current_place.neighbors):
-                neighbor = place_map.placeMap[neighbor_key]
-                print(f"{i+1}. {neighbor.name}")
-
-            choice = int(input("Enter the number of the neighbor you want to go to: "))
-            if choice >= 1 and choice <= len(current_place.neighbors):
-                chosen_neighbor = current_place.neighbors[choice - 1]
-                print(f"Moving to {place_map.placeMap[chosen_neighbor].name}...\n")
-                traverse(place_map, chosen_neighbor)
-            else:
-                print("Invalid neighbor choice.\n")
-
-        if current_place.occupants:
-            print("Occupants: ")
-            for occupant in current_place.occupants:
-                print(f"- {occupant}")
-
-        print()
-
-    else:
+    if place_key not in place_map:
         print(f"Invalid place key: {place_key}")
+        return
+
+    current_place = place_map.placeMap[place_key]
+    print(f"You are in {current_place.name}.")
+
+    if current_place.exit:
+        print("This place has an exit.")
+
+    if current_place.neighbors:
+        print("Neighbors: ")
+        for i, neighbor_key in enumerate(current_place.neighbors):
+            neighbor = place_map.placeMap[neighbor_key]
+            print(f"{i+1}. {neighbor.name}")
+
+        choice = int(input("Enter the number of the neighbor you want to go to: "))
+        if choice >= 1 and choice <= len(current_place.neighbors):
+            chosen_neighbor = current_place.neighbors[choice - 1]
+            print(f"Moving to {place_map.placeMap[chosen_neighbor].name}...\n")
+            traverse(place_map, chosen_neighbor)
+        else:
+            print("Invalid neighbor choice.\n")
+
+    # if current_place.occupants:
+    #     print("Occupants: ")
+    #     for occupant in current_place.occupants:
+    #         print(f"- {occupant}")
+
+    print()
 
 
 def main():
