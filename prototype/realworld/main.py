1b7166c6add368ad601a84e45df836a46a785753
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-31 18:14:04 -0400

    real world context based flash cards

--- prototype/realworld/main.py
+++ prototype/realworld/main.py
@@ -1,6 +1,21 @@
+import random
 from dataclasses import dataclass, asdict
 from termcolor import cprint
 import jsoneng
+
+jdb = jsoneng.JsonDB()
+jdb.create({})
+
+
+@dataclass
+class Event:
+    key: str
+    desc: str
+
+
+@dataclass
+class EventMap:
+    eventMap: dict
 
 
 @dataclass
@@ -19,7 +34,9 @@ h_livingroom = Place("h_livingroom", "Living Room", ["h_hallway"])
 h_hallway = Place("h_hallway", "Hallway", ["h_livingroom", "h_door"])
 h_door = Place("h_door", "House Front Door", ["h_hallway", "street"])
 
-sm_entrance = Place("sm_entrance", "Supermarket Entrance", ["sm_checkout", "sm_aisle"])
+sm_entrance = Place(
+    "sm_entrance", "Supermarket Entrance", ["street", "sm_checkout", "sm_aisle"]
+)
 sm_checkout = Place("sm_checkout", "Supermarket Checkout", ["sm_entrance", "sm_aisle"])
 sm_aisle = Place(
     "sm_aisle",
@@ -38,6 +55,48 @@ sm_vegetables = Place("sm_vegetables", "Vegetables", ["sm_aisle"])
 
 street = Place("street", "A Quiet Street", ["h_door", "sm_entrance"])
 
+h_dinner = Event(
+    "h_dinner",
+    "You are hungry. What do you do?",
+)
+
+h_momcallsfordinner = Event(
+    "h_momcallsfordinner",
+    "Your mom calls for dinner, what do you do?",
+)
+
+h_momcallsforlunch = Event(
+    "h_momcallsforlunch",
+    "Your mom calls for lunch, what do you do?",
+)
+
+h_pee = Event(
+    "h_pee",
+    "What do you do when you pee?",
+)
+
+h_boredom = Event(
+    "h_boredom",
+    "What do you do when you are bored?",
+)
+
+sm_deal = Event(
+    "sm_deal",
+    "You spot some good deals at the supermarket?",
+)
+
+sm_pee = Event(
+    "sm_pee",
+    "What do you do when you need to pee at the supermarket?",
+)
+
+
+h_events = [h_dinner, h_momcallsfordinner, h_momcallsforlunch, h_pee, h_boredom]
+
+sm_events = [sm_deal, sm_pee]
+
+street_events = []
+
 w_map = PlaceMap(
     {
         "h_livingroom": h_livingroom,
@@ -53,32 +112,64 @@ w_map = PlaceMap(
     },
 )
 
+e_map = EventMap(
+    {
+        "h_livingroom": h_events,
+        "h_hallway": h_events,
+        "h_door": h_events,
+        "sm_entrance": sm_events,
+        "sm_checkout": sm_events,
+        "sm_aisle": sm_events,
+        "sm_bakery": sm_events,
+        "sm_butchers": sm_events,
+        "sm_vegetables": sm_events,
+        "street": street_events,
+    }
+)
+
 
 def traverse(place_map, place_key):
     """
     Traverses through the place map starting from the given place key.
     """
 
-    if place_key not in place_map.placeMap:
-        print(f"Invalid place key: {place_key}")
-        return
-
-    current_place = place_map.placeMap[place_key]
-    cprint(f"You are in {current_place.name}.", "red")
-
-    if current_place.neighbors:
-        print("Places You Can Visit: ")
-        for i, neighbor_key in enumerate(current_place.neighbors):
-            neighbor = place_map.placeMap[neighbor_key]
-            cprint(f"{i+1}. {neighbor.name}", "red")
-
-        choice = int(input("Enter the number of the neighbor you want to go to: "))
-        if choice >= 1 and choice <= len(current_place.neighbors):
-            chosen_neighbor = current_place.neighbors[choice - 1]
-            print(f"Moving to {place_map.placeMap[chosen_neighbor].name}...\n")
-            traverse(place_map, chosen_neighbor)
+    time = 0
+    while True:
+        time += 1
+        if place_key not in place_map.placeMap:
+            print(f"Invalid place key: {place_key}\n")
+            place_key = input("Enter a valid place key: ")
         else:
-            print("Invalid neighbor choice.\n")
+            current_place = place_map.placeMap[place_key]
+            cprint(f"You are in {current_place.name}.", "red")
+            cprint(f"The time is {time}.", "blue")
+
+            events = e_map.eventMap.get(current_place.key, [])
+
+            if events:
+                event = random.choice(events)
+                cprint(f"Event: {event.desc}.\n", "yellow")
+
+                text = input("> ")
+                jdb.patch({time: {"prompt": event.desc, "answer": text}})
+            else:
+                cprint(f"There are no events in {current_place.name}.\n", "yellow")
+
+            if current_place.neighbors:
+                print("Places You Can Visit: ")
+                for i, neighbor_key in enumerate(current_place.neighbors):
+                    neighbor = place_map.placeMap[neighbor_key]
+                    cprint(f"{i+1}. {neighbor.name}", "red")
+
+                choice = input("Enter the number of the neighbor you want to go to: ")
+                if choice.isdigit() and 1 <= int(choice) <= len(
+                    current_place.neighbors
+                ):
+                    chosen_neighbor = current_place.neighbors[int(choice) - 1]
+                    print(f"Moving to {place_map.placeMap[chosen_neighbor].name}...\n")
+                    place_key = chosen_neighbor
+                else:
+                    print("Invalid neighbor choice.\n")
 
     # if current_place.occupants:
     #     print("Occupants: ")
@@ -92,9 +183,6 @@ def main():
     print("begins program")
 
     # Example usage: Traverse through the w_map starting from the "street_map"
-
-    jdb = jsoneng.JsonDB()
-    jdb.create({})
 
     jdb.update(asdict(w_map))
     traverse(w_map, "h_livingroom")
