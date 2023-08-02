1fac8a3ee981ff975a1cda8bdd169277adaaa928
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-08-02 11:40:14 -0400

    travel

--- prototype/realworld/attributes.py
+++ prototype/realworld/attributes.py
@@ -24,13 +24,12 @@ h_livingroom = Place("h_livingroom", "Living Room", ["h_hallway"])
 h_hallway = Place("h_hallway", "Hallway", ["h_livingroom", "h_door"])
 h_door = Place("h_door", "House Front Door", ["h_hallway", "street"])
 
-h_dinner = Event(
-    "h_dinner",
-    "You are hungry. What do you do?",
-)
-
 h_events = [
-    h_dinner,
+    Event("h_hunger", "You are hungry. What do you do?"),
+    Event("h_thirst", "You are thirsty. What do you do?"),
+    Event("h_tired", "You are tired. What do you do?"),
+    Event("h_happy", "You are happy. What do you do?"),
+    Event("h_sad", "You are sad. What do you do?"),
 ]
 
 p_map = {
@@ -41,30 +40,23 @@ p_map = {
 
 
 def travel():
-    # def getNeighbors(place_key):
-    #     neighbors = []
-    #     for neighbor in place_map[place_key]:
-    #         neighbors.append(neighbor)
-    #     return neighbor
-
-    
-
+    currentPlace = h_livingroom
     while True:
-        for i in range(len(neighbors)):
-            cprint(f"{i} {neighbors[i].name}.", "red")
+        for i in range(len(currentPlace.neighbors)):
+            cprint(f"{i}. {p_map[currentPlace.neighbors[i]][0].name}.", "red")
 
         userInput = input("enter place: > ")
 
         if userInput == "exit":
             break
 
-        if userInput in p_map:
-            current_place = p_map[userInput][0]
-            cprint(f"You are in {current_place.name}.", "red")
+        # if userInput in p_map:
+        #     current_place = p_map[userInput][0]
+        #     cprint(f"You are in {current_place.name}.", "red")
 
 
 def resolveCombat(p1, p2, place, event):
     pass
 
 
-travel(p_map, "h_livingroom")
+travel()
