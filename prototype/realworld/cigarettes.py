a8adcecaad2e2e396f05b354cf033eda2ee82e47
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-08-02 19:31:53 -0400

    game refactored

--- prototype/realworld/cigarettes.py
+++ prototype/realworld/cigarettes.py
@@ -52,57 +52,73 @@ p_map = {
     "school": school,
 }
 
-time = 0
-currentPlace = home
-while True:
-    # print general information
+
+def newDay(time, currentPlace):
     cprint(f"a new day, day: {time}, and you are in {currentPlace.name}", "blue")
     cprint("available actions:", "green")
     cprint("0. rest", "red")
     cprint("1. move", "red")
     cprint("2. events", "red")
     action = input("enter action: > ")
+    return action
 
-    if action == "0":
-        print("you rested")
 
-    elif action == "1":
-        print("move where?")
+def changeCurrentPlace(time, currentPlace):
+    cprint("available neighbors:", "green")
+    for i in range(len(currentPlace.neighbors)):
+        cprint(f"{i}. {p_map[currentPlace.neighbors[i]].name}.", "red")
 
-        # list neighbors
-        cprint("available neighbors:", "green")
-        for i in range(len(currentPlace.neighbors)):
-            cprint(f"{i}. {p_map[currentPlace.neighbors[i]].name}.", "red")
-
-        # choose neighbor
-        choice = input("enter location: > ")
-        if choice.isdigit() and 0 <= int(choice) <= len(currentPlace.neighbors) - 1:
-            chosen_neighbor = currentPlace.neighbors[int(choice)]
-            cprint(f"Moving to {p_map[chosen_neighbor].name}...", "red")
-            currentPlace = p_map[chosen_neighbor]
-        else:
-            cprint("Invalid neighbor choice.", "red")
-
-    elif action == "2":
-        if currentPlace.events:
-            # get event
-            cprint("you get an event:", "green")
-            currentEvent = random.choice(currentPlace.events)
-            currentEvent.action()
-
-            # ask player to describe in diary
-            cprint("write down what happened please", "red")
-            text = input("> ")
-            jdb.patch({time: {"prompt": currentEvent.desc, "answer": text}})
-            print("")
-        else:
-            cprint("no available events", "red")
-
+    # choose neighbor
+    choice = input("enter location: > ")
+    if choice.isdigit() and 0 <= int(choice) <= len(currentPlace.neighbors) - 1:
+        chosen_neighbor = currentPlace.neighbors[int(choice)]
+        cprint(f"Moving to {p_map[chosen_neighbor].name}...", "red")
+        currentPlace = p_map[chosen_neighbor]
     else:
-        cprint("invalid action", "red")
+        cprint("Invalid neighbor choice.", "red")
 
-    input("time passes...\n")
-    time += 1
-    print("")
-    print("")
-    print("")
+
+def getEvents(time, currentPlace):
+    if currentPlace.events:
+        # get event
+        cprint("you get an event:", "green")
+        currentEvent = random.choice(currentPlace.events)
+        currentEvent.action()
+
+        # ask player to describe in diary
+        cprint("write down what happened please", "red")
+        text = input("> ")
+        jdb.patch({time: {"prompt": currentEvent.desc, "answer": text}})
+        print("")
+    else:
+        cprint("no available events", "red")
+
+
+def main(time, currentPlace):
+    while True:
+        action = newDay(time, currentPlace)
+
+        if action == "exit":
+            print("thank you for playing")
+            break
+
+        if action == "0":
+            print("you rested")
+
+        elif action == "1":
+            print("move where?")
+
+            currentPlace = changeCurrentPlace(time, currentPlace)
+
+        elif action == "2":
+            getEvents(time, currentPlace)
+
+        else:
+            cprint("invalid action", "red")
+
+        input("time passes...\n")
+        time += 1
+
+
+if __name__ == "__main__":
+    main(0, p_map["home"])
