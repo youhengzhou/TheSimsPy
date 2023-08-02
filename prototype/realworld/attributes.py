e06a52ec8eba7735c3e13871013f8ad3d7e352bb
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-08-02 19:26:09 -0400

    cigarettes

--- prototype/realworld/attributes.py
+++ prototype/realworld/attributes.py
@@ -24,6 +24,11 @@ h_livingroom = Place("h_livingroom", "Living Room", ["h_hallway"])
 h_hallway = Place("h_hallway", "Hallway", ["h_livingroom", "h_door"])
 h_door = Place("h_door", "House Front Door", ["h_hallway", "street"])
 
+street = Place("street", "Street", ["h_door", "s_door"])
+
+s_door = Place("s_door", "School Door", ["street", "s_hallway"])
+s_hallway = Place("s_hallway", "School Hallway", ["s_door"])
+
 h_events = [
     Event("h_hunger", "You are hungry. What do you do?"),
     Event("h_thirst", "You are thirsty. What do you do?"),
@@ -36,6 +41,9 @@ p_map = {
     "h_livingroom": [h_livingroom, h_events],
     "h_hallway": [h_hallway, h_events],
     "h_door": [h_door, h_events],
+    "street": [street, []],
+    "s_door": [s_door, []],
+    "s_hallway": [s_hallway, []],
 }
 
 
@@ -45,14 +53,17 @@ def travel():
         for i in range(len(currentPlace.neighbors)):
             cprint(f"{i}. {p_map[currentPlace.neighbors[i]][0].name}.", "red")
 
-        userInput = input("enter place: > ")
+        choice = input("enter place: > ")
 
-        if userInput == "exit":
+        if choice == "exit":
             break
 
-        # if userInput in p_map:
-        #     current_place = p_map[userInput][0]
-        #     cprint(f"You are in {current_place.name}.", "red")
+        if choice.isdigit() and 0 <= int(choice) <= len(currentPlace.neighbors) - 1:
+            chosen_neighbor = currentPlace.neighbors[int(choice)]
+            print(f"Moving to {p_map[chosen_neighbor][0].name}...\n")
+            currentPlace = p_map[chosen_neighbor][0]
+        else:
+            print("Invalid neighbor choice.\n")
 
 
 def resolveCombat(p1, p2, place, event):
---
+++ prototype/realworld/cigarettes.py
@@ -0,0 +1,108 @@
+import random
+from dataclasses import dataclass, asdict
+from termcolor import cprint
+import jsoneng
+
+jdb = jsoneng.JsonDB()
+jdb.create({})
+
+
+@dataclass
+class Char:
+    name: str
+    role: str
+    authenciticy: int
+    resolve: int
+    inspiration: int
+    personality: int
+
+
+@dataclass
+class Event:
+    key: str
+    desc: str
+    action: None
+
+
+@dataclass
+class Place:
+    key: str
+    name: str
+    neighbors: list
+    events: list
+
+
+def testAction():
+    print("what's 1 + 1?")
+    answer = input("> ")
+    if answer == "2":
+        print("you win!")
+        return
+    print("you lose!")
+
+
+test = Event("test", "you have a test coming up, what do you do?", testAction)
+
+
+home = Place("home", "your home", ["school"], [])
+school = Place("school", "your school", ["home"], [test])
+
+p_map = {
+    "home": home,
+    "school": school,
+}
+
+time = 0
+currentPlace = home
+while True:
+    # print general information
+    cprint(f"a new day, day: {time}, and you are in {currentPlace.name}", "blue")
+    cprint("available actions:", "green")
+    cprint("0. rest", "red")
+    cprint("1. move", "red")
+    cprint("2. events", "red")
+    action = input("enter action: > ")
+
+    if action == "0":
+        print("you rested")
+
+    elif action == "1":
+        print("move where?")
+
+        # list neighbors
+        cprint("available neighbors:", "green")
+        for i in range(len(currentPlace.neighbors)):
+            cprint(f"{i}. {p_map[currentPlace.neighbors[i]].name}.", "red")
+
+        # choose neighbor
+        choice = input("enter location: > ")
+        if choice.isdigit() and 0 <= int(choice) <= len(currentPlace.neighbors) - 1:
+            chosen_neighbor = currentPlace.neighbors[int(choice)]
+            cprint(f"Moving to {p_map[chosen_neighbor].name}...", "red")
+            currentPlace = p_map[chosen_neighbor]
+        else:
+            cprint("Invalid neighbor choice.", "red")
+
+    elif action == "2":
+        if currentPlace.events:
+            # get event
+            cprint("you get an event:", "green")
+            currentEvent = random.choice(currentPlace.events)
+            currentEvent.action()
+
+            # ask player to describe in diary
+            cprint("write down what happened please", "red")
+            text = input("> ")
+            jdb.patch({time: {"prompt": currentEvent.desc, "answer": text}})
+            print("")
+        else:
+            cprint("no available events", "red")
+
+    else:
+        cprint("invalid action", "red")
+
+    input("time passes...\n")
+    time += 1
+    print("")
+    print("")
+    print("")
